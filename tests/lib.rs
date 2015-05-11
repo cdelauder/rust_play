39fbcb7b8215d09dbd044f1a32aa10eb4d8e23ad
cdelauder <cdelauder@gmail.com>
2015-05-10 20:17:24 -0700

    I wrote fizzbuzz in rust

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,10 +1,12 @@
-
+/// this is the function we are testing
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+/// this is how to build a test module
 #[cfg(test)]
 mod tests {
+  /// pull in all of the public functions
   use super::*;
   #[test]
   fn test_two() {
