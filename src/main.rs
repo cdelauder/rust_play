9a20ef991fb4c6a78808e6b3b4ae43520409e547
cdelauder <cdelauder@gmail.com>
2015-05-10 20:35:28 -0700

    now it runs with user input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-fn fizz() {
-  for i in (1..100) {
+fn fizz(x: i32) {
+  for i in (1..x+1) {
     if i % 15 == 0 {
       println!("fizzbuzz");
     } else if i % 5 == 0 {
@@ -13,5 +13,20 @@ fn fizz() {
 }
 
 fn main() {
-  fizz();
+  use std::io;
+  println!("how many numbers would you like to fizzbuzz?");
+  let mut limit = String::new();
+  io::stdin().read_line(&mut limit)
+      .ok()
+      .expect("failed to get the input");
+
+  let limit: i32 = match limit.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("let's keep it a number you putz");
+        return;
+      }
+    };
+  println!("starting fizzbuzz for limit {}", limit);
+  fizz(limit);
 }
